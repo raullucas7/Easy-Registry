db190acde1b711d04cdce8b9cf8abf0058979cda
raul <raullinjetmc@gmail.com>
2024-06-24 08:26:26 -0500

    multiple functions implemented: view, add, remove, leave

--- todo.py
+++ todo.py
@@ -1,41 +1,70 @@
 import os
 
+
 def menu_interface():
     print("Welcome to your to-do list.")
-    print("-> View list")
-    print("-> Add list element")
-    print("-> Remove list element")
-    print("-> Leave")
-
-def view_list():
+    print("1. View list")
+    print("2. Add list element")
+    print("3. Remove list element")
+    print("4. Leave")
 
 
-def add_element():
+def view_list(todo_list):
+    if not todo_list:
+        print("-> List is empty")
+    
+    else:
+        print("-> Your list:")
+        for idx, item in enumerate(todo_list, start=1):
+            print(f"{idx}. {item}")
 
 
+def add_element(todo_list):
+    item = input("Enter a to-do item: ")
+    todo_list.append(item)
+    
+    print(f"'{item}' has been added to your to-do list.")
+
+def remove_element(todo_list):
+    view_list(todo_list)
+    if todo_list:
+        try:
+            item_number = int(input("Enter the number of the item to remove: "))
+            
+            if 1 <= item_number <= len(todo_list):
+                removed_item = todo_list.pop(item_number - 1)
+                print(f"'{removed_item}' has been removed from your to-do list.")
+            
+            else:
+                print("Invalid item number.")
+        
+        except ValueError:
+            print("Please enter a valid number.")
 
 
-def remove_element():
+def save_list(filename, todo_list):
+    with open(filename, 'w') as file:
+        
+        for item in todo_list:
+            file.write(item + '\n')
+    
+    print("List saved.")
 
-
-
-
-def save_list():
-
-
-
-
-def load_list():
-
-
-
+def load_list(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            todo_list = [line.strip() for line in file.readlines()]
+        
+        print("List loaded.")
+        return todo_list
+    
+    else:
+        return []
 
 def main():
-    
     todo_list = load_list("todo_list.txt")
-
     while True:
-        show_menu()
+        menu_interface()
         option = input("Enter your choice: ")
         
         if option == '1':
@@ -48,10 +77,11 @@ def main():
             remove_element(todo_list)
         
         elif option == '4':
+            save_list("todo_list.txt", todo_list)
             break
         
         else:
-            print("Error in choice. Try again.")
-        
+            print("Invalid choice. Try again.")
+
 if __name__ == "__main__":
     main()
